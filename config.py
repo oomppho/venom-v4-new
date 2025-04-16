b83cd444a4a30402ef79ed8241dd908ded4b386c
oomppho <sisodiasonia44@gmail.com>
2025-04-16 10:02:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7237699304:AAEm3dV2ycRQldfqJNUYh2DVF4T6_xI-lpw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("HarshRajput204")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5486548107")
 
 
 
